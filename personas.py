300baa357bd89ade8c7c2986858774fbb4df6393
tgastaldello <tgastaldello@itba.edu.ar>
2023-10-10 19:18:33 -0300

    Clase Personal

--- personas.py
+++ personas.py
@@ -19,14 +19,5 @@ class Persona():
         else:  return False
         
     
-class Personal(Persona):
-    def __init__(self,nombre,dni,direccion,contacto,fecha_nac):
-        super().__init__(nombre,dni,direccion,contacto,fecha_nac)
-    #def altas(self):
-        # cuando se contrata un nuevo empleado que se agregue a la estructura de datos utlizada
-    #def bajas(self):
-        # cuando se despide un empleado que se agregue a la estructura de datos utlizada
-    # def tareas(self):
-    # def ingreso(self):
-    # def egreso (self):
+
         
