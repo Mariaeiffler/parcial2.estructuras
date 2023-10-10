083b1fc9cba3c5343e62b1cc99e563213a09e164
tgastaldello <tgastaldello@itba.edu.ar>
2023-10-10 19:17:06 -0300

    cliente

---
+++ cliente.py
@@ -0,0 +1,7 @@
+class Cliente(Persona):
+    def __init__(self,nombre,dni,direccion,contacto,fecha_nac):
+        super().__init__(nombre,dni,direccion,contacto,fecha_nac)
+    #def tipo(self):
+        #verificar que tipo de cliente es en base a sus gastos
+    #def historial_reservas(self):
+    #def __str__(self):
--- personas.py
+++ personas.py
@@ -18,13 +18,6 @@ class Persona():
                 return True
         else:  return False
         
-class Cliente(Persona):
-    def __init__(self,nombre,dni,direccion,contacto,fecha_nac):
-        super().__init__(nombre,dni,direccion,contacto,fecha_nac)
-    #def tipo(self):
-        #verificar que tipo de cliente es en base a sus gastos
-    #def historial_reservas(self):
-    #def __str__(self):
     
 class Personal(Persona):
     def __init__(self,nombre,dni,direccion,contacto,fecha_nac):
