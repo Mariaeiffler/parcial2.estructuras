e344dfbb43770a476052b43de9bcd36bdc17d0bf
tgastaldello <tgastaldello@itba.edu.ar>
2023-10-10 19:06:34 -0300

    Agrege la clase

---
+++ personas.py
@@ -0,0 +1,39 @@
+class Persona():
+    def _init_(self,nombre,dni,direccion,contacto,fecha_nac):
+        self.nombre=nombre
+        self.dni=dni
+        self.direccion=direccion
+        self.contacto=contacto
+        self.fecha_nac=fecha_nac
+        
+    def _str_(self):
+        cadena=''
+        cadena='La persona llamada {}, nacio el {}, tiene DNI {}, su direccion es {} y la informacion de contacto es {}'.format(self.nombre,self.fecha_nac,self.dni,self.direccion,self.contacto)
+        return cadena
+    
+    def _eq_(self, personita): #es necesario?
+        if id(self)==id(personita):
+             return True
+        elif self.ident==personita.ident:
+                return True
+        else:  return False
+        
+class Cliente(Persona):
+    def __init__(self,nombre,dni,direccion,contacto,fecha_nac):
+        super().__init__(nombre,dni,direccion,contacto,fecha_nac)
+    #def tipo(self):
+        #verificar que tipo de cliente es en base a sus gastos
+    #def historial_reservas(self):
+    #def __str__(self):
+    
+class Personal(Persona):
+    def __init__(self,nombre,dni,direccion,contacto,fecha_nac):
+        super().__init__(nombre,dni,direccion,contacto,fecha_nac)
+    #def altas(self):
+        # cuando se contrata un nuevo empleado que se agregue a la estructura de datos utlizada
+    #def bajas(self):
+        # cuando se despide un empleado que se agregue a la estructura de datos utlizada
+    # def tareas(self):
+    # def ingreso(self):
+    # def egreso (self):
+        
