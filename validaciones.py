f60026a968ea29c8962cad030fc97e43f0f46426
mariaeiffler <meiffler@itba.edu.ar>
2023-10-17 19:07:15 -0300

    mas validaciones (4)

--- validaciones.py
+++ validaciones.py
@@ -13,105 +13,63 @@ def validaciondni(dni):
     ## agregar tambien que no se repita
     return dni
 
-def validacioncontraseña1(contraseña):
-    mayuscula = False
-    numero = False
-    for digito in contraseña:
-        if digito.isdigit() == True:
-            numero = True
+def cantidad_mayusculas(contrasena):
+    contador = 0
+    for digito in contrasena:
         if digito.isupper() == True:
-            mayuscula = True
-    return mayuscula, numero
+            contador += 1
+    return contador
 
-def validacioncontraseña2(contraseña):
-    mayuscula, numero = validacioncontraseña1(contraseña)
-    if mayuscula == False or numero == False:
-        contraseña = input ('Ingrese una contraseña valida ')
-        mayuscula, numero = validacioncontraseña1(contraseña)
-        while mayuscula == False or numero == False:
-            contraseña = input ('Ingrese una contraseña valida ')
-            mayuscula, numero = validacioncontraseña1(contraseña)
-        return contraseña
-    verificacion = True
-    return verificacion
+def cantidad_numero(contrasena):
+    contador = 0
+    for digito in contrasena:
+        if digito.isdigit() == True:
+            contador +=1
+    return contador
+            
+def validacioncontrasena(contrasena):
+    if cantidad_mayusculas(contrasena) < 1  or cantidad_numero(contrasena) <1:
+        contrasena = input ('Ingrese una contraseña valida ')
+        while cantidad_mayusculas(contrasena) < 1  or cantidad_numero(contrasena) <1:
+            contrasena = input ('Ingrese una contraseña valida ')
+    return contrasena
+
 
 def validacionempleado(soy_empleado):
-    if soy_empleado != 'si' or soy_empleado != 'no':
-        print ('hola')
+    if soy_empleado != 'si' and soy_empleado != 'no':
         soy_empleado = input('Ingrese si, si es empleado y no si no lo es (en minuscula) ')
-        while soy_empleado != 'si' or soy_empleado != 'no':
-            print ('chau')
+        while soy_empleado != 'si' and soy_empleado != 'no':
             soy_empleado = input('Ingrese si, si es empleado y no si no lo es (en minuscula) ')
     if soy_empleado == 'si':
         return True
     else:
         return False
-
-soy_empleado = 'si'
-validacion = validacionempleado(soy_empleado)
-
-
-
-
-
-
-##########################################  ARCHIVOS
-
-# import csv
-
-# def abrirArchivo(archivo):
-#     lista = []
-#     try:
-#         with open(archivo, 'r', encoding='utf-8') as archivo:
-#             lector = csv.reader(archivo)                       
-#             for fila in lector:                        
-#                 lista.append(fila)
-#         return (lista)
-        
-#     except FileNotFoundError:
-#         print("El archivo", 'FILE', "no existe.")
-#     return
-
-# try:
-#     fn = input('Ingrese el nombre del archivo a abrir: ')
-#     fd = None
-#     fd = open( fn , 'r')
-#     line = fd.readline()
-# except FileNotFoundError:
-#     print('El archivo no existe. Ingrese otro archivo.')
-# finally:
-#     if fd:
-#         print('cerrando el archivo')
-#         fd.close()
-
-
-# def leer_archivo( fn ):
-#     fd = open( fn , 'r' )
-#     cont = fd.read()
-#     fd.close()
-#     return cont
-
-# def escribir_archivo( fname , contenido ):
-#     fd = open( fname , 'w' )
-#     for linea in contenido:
-#         fd.write(' '.join(linea)+'\n')
-#     fd.close()
     
-# with open('FILE', 'r', encoding='utf-8') as archivo:   # 1
-#   lector = csv.reader(archivo)                       # 2
-#   for fila in lector:                                # 3
-#     las_capitales.append(fila)
-# print(las_capitales) 
+def validacionnombre1(nombre):
+    validacion = True
+    for digito in nombre:
+        if digito.isalpha == False or digito.whitespace == False:
+            validacion = False
+    return validacion
+    
+def validacionnombre2(nombre):
+    validacion = validacionnombre1(nombre)
+    if validacion == False:
+        nombre = input('Ingrese su nombre y apellido ')
+        validacion = validacionnombre1(nombre)
+        while validacion == False:
+            nombre = input('Ingrese su nombre y apellido ')
+            validacion = validacionnombre1(nombre)
+    return nombre
+            
+            
 
-# try: # Probamos el siguiente código
-#     with open('FILE', 'r', encoding='utf-8') as archivo:
-#         lector = csv.reader(archivo)
-#         for fila in lector:
-#             las_capitales.append(fila)
-    
-# except FileNotFoundError:  # Si se encuentra un error se ejecuta esta parte
-#     print("El archivo", 'FILE', "no existe.")
-    
-# else: # De no encontrarse ningún error luego del bloque try continuamos en el else
-#     print(las_capitales) 
+soy_empleado = 'xdertyjgfr5678'
+validacion = validacioncontrasena(soy_empleado)
+print(validacion)
+
+
+
+
+
 
