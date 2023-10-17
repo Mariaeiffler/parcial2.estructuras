d1b9c0da31aa1725212c7380b4f4d8ef146f44cc
tgastaldello <tgastaldello@itba.edu.ar>
2023-10-17 19:45:44 -0300

    hotel y main con pickle

--- hotel.py
+++ hotel.py
@@ -44,4 +44,7 @@ class Hotel():
                 #validar que exista el usuario y que la contrasena sea correcta
                 usuario=input('Escriba el nombre de usuario:')
                 contrasena=input('Escriba la contrasena:')
-                
+    def save(self):
+        with open('hotel.pickle','wb') as f:
+            pickle.dump(self,f)
+    #hacer una funcion para mostrar que el hotel esta guardando informacion
