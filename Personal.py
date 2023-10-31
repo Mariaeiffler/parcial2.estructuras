a0ca3b3055a9e9df49cc7d8d46390afe27198627
MilagrosArg <margibaymolina@itba.edu.ar>
2023-10-31 16:29:26 -0300

    avance tareas

--- Personal.py
+++ Personal.py
@@ -1,16 +1,24 @@
 from datetime import datetime
 from Persona import Persona
+from nodo import Nodo
+from list_enlazada import Lista_Enlazada
+from Tareas_Emplados import tareas_empleados
+
 class Personal(Persona):
     def __init__(self,nombre,dni,direccion,contacto,fecha_nac,mail,soy_empleado,fecalta,tipo,fecbaja: None):
         super().__init__(nombre,dni,direccion,contacto,fecha_nac,mail,soy_empleado)
         self.fecalta = datetime.now()
         self.tipo=tipo
+        self.Tareas=Lista_Enlazada()
         
     def bajas(self):
         self.fechabaja = datetime.now()
 
-    # def tareas(self):
-    
+    def tareas(self): 
+        print(tareas_empleados)
+        opcion=input('Ingrese el número de tarea que quiere asignar: ') #chequear que la opcion sea la correcta
+        prioridad= input ('Ingrese la prioridad (1,2 o 3): ') #chequear que sea 1,2 o 3
+        
     # def ingreso(self):
     
     # def egreso (self):
