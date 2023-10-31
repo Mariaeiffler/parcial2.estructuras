ffb0cc175cd8119b295928fb2e61456f93b58da0
milemaceri <mmaceri@itba.edu.ar>
2023-10-31 19:44:33 -0300

    buff

--- cliente.py
+++ cliente.py
@@ -1,11 +1,17 @@
 from Persona import Persona
-class Cliente(Persona):
+from hotel import *
+
+class Cliente(Persona,Hotel):
     def __init__(self,nombre,usuario,dni,direccion,contacto,fecha_nac,mail,soy_empleado,contrasena,tipo='nivel 1', reservas = []):
         super().__init__(nombre,dni,direccion,contacto,fecha_nac,mail,soy_empleado)
         self.usuario = usuario
         self.contrasena = contrasena
         self.tipo=tipo
         self.reservas= reservas
+    
+    def realizar_pedido():
+        
+        
         
     #def tipo(self):
         #verificar que tipo de cliente es en base a sus gastos
