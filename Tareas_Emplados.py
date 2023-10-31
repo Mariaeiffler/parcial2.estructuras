93e70717c8e3a012176e53f5d6a47d8a0eb80cea
MilagrosArg <margibaymolina@itba.edu.ar>
2023-10-31 11:13:22 -0300

    lista tareas empleados

---
+++ Tareas_Emplados.py
@@ -0,0 +1,26 @@
+diccionario = {
+    "Lavar los platos": 'personal de cocina',
+    'Cocinar desayuno': 'personal de cocina',
+    "Cocinar almuerzo": 'personal de cocina',
+    "Cocinar merienda": 'personal de cocina',
+    "Cocinar cena": 'personal de cocina',
+    "Limpieza de las habitaciones 1-10": 'personal de limpieza',
+    "Limpieza de las habitaciones 11-20": 'personal de limpieza',
+    'Limpieza de las habitaciones 21-30': 'personal de limpieza',
+    "Limpieza de las habitaciones 31-40": 'personal de limpieza',
+    'Limpieza de las habitaciones 41-50': 'personal de limpieza',
+    'Mantenimiento de la habitación...': 'personal de mantenimiento', 
+    'Mantenimiento de espacio común...': 'personal de mantenimiento',
+    'Limpieza de la pileta': 'personal de mantenimiento',
+    'Cortar el pasto': 'personal de mantenimiento',
+    'Atender la recepcion': 'personal de recepcion',
+    'Mandar a lavar las toallas y la ropa de cama': 'personal de limpieza',
+    'Ayudar a los huespedes con sus preguntas': 'personal de recepcion',
+    'Ayudar a los huespedes a seleccionar actividades para realizar': 'personal de recepcion',
+    'Atender las quejas y preocupaciones de los clientes': 'personal de recepcion',
+    'Organizacion de eventos para huespedes': 'personal de recepcion',
+    'Coordinar servicios de transporte para los huespedes': 'personal de recepcion'
+}
+
+#agregar para que asignen valor de importancia 
+# generar metodo en hotel donde el servidor ppl pueda agregar mas tareas al diccionario de tareas
