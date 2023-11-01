b5a25b1352513f957ee64c2a4019475e89781cbd
milemaceri <mmaceri@itba.edu.ar>
2023-11-01 16:01:29 -0300

    comment

--- cliente.py
+++ cliente.py
@@ -1,7 +1,7 @@
 from Persona import Persona
-from hotel import *
+from Buffet import *
 
-class Cliente(Persona,Hotel):
+class Cliente(Persona):
     def __init__(self,nombre,usuario,dni,direccion,contacto,fecha_nac,mail,soy_empleado,contrasena,tipo='nivel 1', reservas = []):
         super().__init__(nombre,dni,direccion,contacto,fecha_nac,mail,soy_empleado)
         self.usuario = usuario
@@ -10,6 +10,12 @@ class Cliente(Persona,Hotel):
         self.reservas= reservas
     
     def realizar_pedido():
+        Buffet.self.mostrar_menu()
+    
+    #aca hay que traer los metodos de buffet para que el cliente pueda hacer los pedidos y
+    # en la clase personal, el cocinero debería hacer el pedido. 
+    # Una vez que se hace un pedido, se debería agregar a las tareas del personal de cocina. 
+    
         
         
         
