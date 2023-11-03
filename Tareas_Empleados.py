15df268cdd3e2466f80e379d1a91826b43975d2b
mariaeiffler <meiffler@itba.edu.ar>
2023-11-03 18:42:56 -0300

    corregi algo

--- Tareas_Empleados.py
+++ Tareas_Empleados.py
@@ -1,26 +1,28 @@
 tareas_empleados = {
-    "Lavar los platos": 'personal de cocina',
-    'Cocinar desayuno': 'personal de cocina',
-    "Cocinar almuerzo": 'personal de cocina',
-    "Cocinar merienda": 'personal de cocina',
-    "Cocinar cena": 'personal de cocina',
-    "Limpieza de las habitaciones 1-10": 'personal de limpieza',
-    "Limpieza de las habitaciones 11-20": 'personal de limpieza',
-    'Limpieza de las habitaciones 21-30': 'personal de limpieza',
-    "Limpieza de las habitaciones 31-40": 'personal de limpieza',
-    'Limpieza de las habitaciones 41-50': 'personal de limpieza',
-    'Mantenimiento de la habitación...': 'personal de mantenimiento', 
-    'Mantenimiento de espacio común...': 'personal de mantenimiento',
-    'Limpieza de la pileta': 'personal de mantenimiento',
-    'Cortar el pasto': 'personal de mantenimiento',
-    'Atender la recepcion': 'personal de recepcion',
-    'Mandar a lavar las toallas y la ropa de cama': 'personal de limpieza',
-    'Ayudar a los huespedes con sus preguntas': 'personal de recepcion',
-    'Ayudar a los huespedes a seleccionar actividades para realizar': 'personal de recepcion',
-    'Atender las quejas y preocupaciones de los clientes': 'personal de recepcion',
-    'Organizacion de eventos para huespedes': 'personal de recepcion',
-    'Coordinar servicios de transporte para los huespedes': 'personal de recepcion'
+    'cocina': {'empleados':[],'tareas':["Lavar los platos",'Cocinar desayuno',"Cocinar merienda","Cocinar cena"]},
+    'limpieza': {'empleados':[],'tareas':["Limpieza de las habitaciones 1-3","Limpieza de las habitaciones 4-6",'Limpieza de las habitaciones 7-9',"Limpieza de las habitaciones 10-12"]},
+    'mantenimiento':{'empleados':[],'tareas':['Mantenimiento de habitación','Mantenimiento de espacio común','Limpieza de pileta','Cortar el pasto']},
+    'administrativo': {'empleados':[],'tareas':['Atender la recepcion','Mandar a lavar las toallas y la ropa de cama','Ayudar a los huespedes con sus preguntas','Ayudar a los huespedes a seleccionar actividades para realizar','Atender las quejas y preocupaciones de los clientes', 'Organizacion de eventos para huespedes','Coordinar servicios de transporte para los huespedes']},
+    'gerente': {'empleados':[],'tareas':['Asignar tareas']}
 }
+
+def agregarTareas (diccionario): #comprobar que sea administrador quien agregue la tarea
+    tipo = input('Ingrese a que tipo de empleado desea agregarle la tarea: ')
+    while tipo not in tareas_empleados:
+        tipo = input ('No existe este tipo de empleados, ingrese el tipo de vuelta: ')
+    tarea= input('Ingrese la tarea que desea agregar: ')
+    diccionario[tipo].append(tarea)
+    print ('La nueva tarea se ha  agregado con exito.')
+    
+def agregarTipoEmpleado (diccionario):
+    tipo = input ('ingrese el nuevo tipo de empleado: ')
+    while tipo in diccionario:
+        tipo = input ('Error, ese tipo de empleado ya existe. Ingrese otro tipo de empleado: ')
+    tarea = input ('Ingrese una tarea que realizaría este tipo de empleado: ')
+    diccionario[tipo]= tarea
+    print ('El tipo de empleado fue agregado con exito.')
+    
+
 
 #agregar para que asignen valor de importancia 
 # generar metodo en hotel donde el servidor ppl pueda agregar mas tareas al diccionario de tareas
