89442b0b962ce9f7b4ad897f48a1664fbed20ed1
mariaeiffler <meiffler@itba.edu.ar>
2023-11-04 13:31:18 -0300

    nomina, inventario, detalles

--- pruebaMili.py
+++ pruebaMili.py
@@ -14,14 +14,14 @@ def valTipoEmpleado(tipo,dicc1:dict):
         tipo=input('Error. Ese tipo de empleado no existe. \n Las opciones disponibles son: {} \n Ingrese una de las opciones existentes:'.format (llaves))
     return tipo
 
-mili=np.array([[1,2,3],[4,5,6]])
+mili=np.array([])
 
 if __name__ == "__main__":
-    mi_set = {1, 2, 3, 4, 5}
-    for i in mi_set:
-        print (i)
+    # mi_set = {1, 2, 3, 4, 5}
+    # for i in mi_set:
+    #     print (i)
     print(mili)
-    mili = np.vstack([mili,[7,8,9]])
+    mili = np.append(mili,[7,8,9])
     print(mili)
     print(len(mili))
 
