011527a19fe2acf561f26f5e5713a8eefd6feaaa
mariaeiffler <meiffler@itba.edu.ar>
2023-11-05 20:54:58 -0300

    error

--- Validaciones.py
+++ Validaciones.py
@@ -86,6 +86,24 @@ def validacioncontrasena(contrasena):
         contrasena = input ('Ingrese una contraseña valida, que contenga una mayuscula y un numero: ')
     return contrasena
 
+def infoPersonas (dicc1:dict,dicc2:dict):   
+    nombre=input('Introduzca su nombre y apellido: ')
+    nombre=valNombre2(nombre)
+    dni=input('Ingrese su DNI: ')
+    dni=validaciondni(dni, dicc1)
+    direccion=input('Ingrese su direccion: ')
+    contacto=input('Ingrese su numero de contacto: ')
+    contacto=validacioncontacto(contacto)
+    fecha_nac=input('Ingrese su fecha de nacimiento: (Debe ser mayor de edad para crearse un usuario) ')
+    fecha_nac=validacionfechanac (fecha_nac)
+    mail=input('Ingrese su mail: ')
+    mail=valMail(mail)
+    usuario=input('Escriba el nombre de usuario: ')
+    usuario=validacionusuario(usuario,dicc1,dicc2)
+    contrasena=input('Escriba una contrasena que contenga por lo menos una mayuscula y un numero: ')
+    contrasena=validacioncontrasena(contrasena)
+    return nombre,usuario,dni,direccion,contacto,fecha_nac,mail,contrasena
+
 def valSignIn (dicc1:dict, dicc2:dict):
     validacion=True
     usuario=input('Ingrese su nombre de usuario: ')
