3692f777ba1990839bb986e5126232271e4b018f
milemaceri <mmaceri@itba.edu.ar>
2023-11-07 18:24:27 -0300

    cliente

--- cliente.py
+++ cliente.py
@@ -19,7 +19,7 @@ class Cliente(Persona):
             return('El clientes de nombre {} y dni {} no tiene reservas en el hotel'.format(self.nombre, self.dni))
     
     def realizar_reserva(self, lista, diccionario:dict):
-        ''' Esta funcion le permite al usuario llevar a cabo la reserva. En caso de que la fecha seleccionada no este disponible se le mostraran las fechas en las cuales esta ocupada para que seleccione una opcion valida'''
+        ''' Esta funcion le permite al usuario llevar a cabo la reserva. Se le pedirá al mismo las fechas en las cuales desea realizar su reserva. En caso de que la fecha seleccionada no este disponible se le mostraran las fechas en las cuales esta ocupada para que seleccione una opcion valida. En caso de que ingrese una fecha de salida que no sea coherente con la de entraa, se le volverá a pedir ingresar una fecha. Luego se le pedira que ingrese la habitacion que desea reservar. En caso de que la habitacion seleccionada no este disponible se le mostraran las habitaciones disponibles para que seleccione una opcion valida.'''
         fecha_inicio, fecha_fin, hab = reserva()
         val = hab_ocupada(fecha_inicio, fecha_fin, hab, lista)
         while val==False:
