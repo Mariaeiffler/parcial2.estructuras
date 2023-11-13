3fbbe83d84493ccd144e20c9e5e963ba3c2a158c
MilagrosArg <margibaymolina@itba.edu.ar>
2023-11-13 18:47:31 -0300

    tareas gerente

--- Tareas_Empleados.py
+++ Tareas_Empleados.py
@@ -6,7 +6,7 @@ tareas_empleados = {
     'limpieza': {'empleados':[],'tareas':["Limpieza de las habitaciones 1-3","Limpieza de las habitaciones 4-6",'Limpieza de las habitaciones 7-9',"Limpieza de las habitaciones 10-12"]},
     'mantenimiento':{'empleados':[],'tareas':['Mantenimiento de habitación','Mantenimiento de espacio común','Limpieza de pileta','Cortar el pasto']},
     'administrativo': {'empleados':[],'tareas':['Atender la recepcion','Mandar a lavar las toallas y la ropa de cama','Ayudar a los huespedes con sus preguntas','Ayudar a los huespedes a seleccionar actividades para realizar','Atender las quejas y preocupaciones de los clientes', 'Organizacion de eventos para huespedes','Coordinar servicios de transporte para los huespedes']},
-    'gerente': {'empleados':[],'tareas':['Asignar tareas']}
+    'gerente': {'empleados':[],'tareas':['Asignar tareas','Contratar empleado','Gestionar proyecto']}
 }
 
 
